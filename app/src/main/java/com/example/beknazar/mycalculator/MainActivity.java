bc7427e6878b0f1a413fe3cd7b6067914135e4cb
Beknazar <beknazark@inbox.ru>
2018-06-20 17:41:41 +0600

    "Writing Functions"

--- app/src/main/java/com/example/beknazar/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/beknazar/mycalculator/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
   TextView tvResult;
 
-  String
+  String oper="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +47,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         float num2=0;
         float result=0;
 
-        if (TextUtils.)
+        if (TextUtils.isEmpty(etNum1.getText().toString())  ||    TextUtils.isEmpty(etNum2.getText().toString())) {
+            return;
+
+        }
+            num1 = Float.parseFloat(etNum1.getText().toString());
+            num2 = Float.parseFloat(etNum2.getText().toString());
+         switch (view.getId()) {
+             case R.id.btnAdd:
+                 oper="+";
+                 result = num1 + num2;
+                 break;
+                 case R.id.btnSub:
+                 oper="-";
+                 result = num1 - num2;
+                 break;
+                 case R.id.btnMult:
+                 oper="*";
+                 result = num1 * num2;
+                 break;
+                 case R.id.btnDiv:
+                 oper="/";
+                 result = num1 / num2;
+                 break;
+                 default:
+                 break;
+
+         }
 
     }
 }
