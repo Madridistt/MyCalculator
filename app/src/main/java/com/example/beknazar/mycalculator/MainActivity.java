88620445928eb9dca1e54b850affbd9d9f3ee626
Beknazar <beknazark@inbox.ru>
2018-06-20 17:25:38 +0600

    "Find id elements"

--- app/src/main/java/com/example/beknazar/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/beknazar/mycalculator/MainActivity.java
@@ -2,12 +2,52 @@ package com.example.beknazar.mycalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+  EditText etNum1;
+  EditText etNum2;
+
+  Button btnAdd;
+  Button btnSub;
+  Button btnMult;
+  Button btnDiv;
+
+  TextView tvResult;
+
+  String
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etNum1=(EditText) findViewById(R.id.etNum1);
+        etNum2=(EditText) findViewById(R.id.etNum2);
+
+        btnAdd=(Button) findViewById(R.id.btnAdd);
+        btnSub=(Button) findViewById(R.id.btnSub);
+        btnMult=(Button) findViewById(R.id.btnMult);
+        btnDiv=(Button) findViewById(R.id.btnDiv);
+        tvResult=(TextView) findViewById(R.id.tvResult);
+
+        btnAdd.setOnClickListener(this);
+        btnSub.setOnClickListener(this);
+        btnMult.setOnClickListener(this);
+        btnDiv.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+      float num1=0;
+        float num2=0;
+        float result=0;
+
+        if (TextUtils.)
+
     }
 }
